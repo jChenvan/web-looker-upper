b9c4b93929879b842d8d363e154d32c57c25baaa
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-06-20 14:34:50 -0400

    Add keywords page

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ const { addRows } = require('../lib/DatabaseMethods');
 const searchCurrentSources = require('../lib/searchCurrentSources');
 var router = express.Router();
 
+// Search new sources
 router.post('/new-sources', async function(req,res,next) {
   const {lastSearchDate} = req.body;
   const keywords = JSON.parse(fs.readFileSync('./data/keywords.json'));
@@ -13,6 +14,7 @@ router.post('/new-sources', async function(req,res,next) {
   res.json({data: await searchNewSources(lastSearchDate, keywords)});
 });
 
+// Search existing sources
 router.post('/current-sources', async function(req,res,next) {
   const {lastSearchDate} = req.body;
   const keywords = JSON.parse(fs.readFileSync('./data/keywords.json'));
@@ -20,6 +22,7 @@ router.post('/current-sources', async function(req,res,next) {
   res.json({data: await searchCurrentSources(lastSearchDate, keywords)});
 });
 
+// Add entries to db
 router.post('/update-db', function(req,res) {
   const {data} = req.body;
 
@@ -28,12 +31,37 @@ router.post('/update-db', function(req,res) {
   res.status(200).send();
 });
 
+// Add urls to be monitored
 router.post('/update-urls', function(req,res) {
   const {newUrls} = req.body;
   const urls = JSON.parse(fs.readFileSync('./data/urls.json'));
   newUrls.forEach(url => {if (!urls.includes(url)) urls.push(url)});
 
   fs.writeFileSync('./data/urls.json', JSON.stringify(urls,null,2));
+
+  res.status(200).send();
+});
+
+// Add keywords
+
+router.post('/add-keyword', function(req,res) {
+  const { keyword: newKeyword } = req.body;
+  const keywords = JSON.parse(fs.readFileSync('./data/keywords.json'));
+  const update = !keywords.includes(newKeyword);
+  if (update) keywords.push(newKeyword);
+
+  fs.writeFileSync('./data/keywords.json', JSON.stringify(keywords,null,2));
+
+  res.json({update});
+});
+
+// Remove keywords
+
+router.post('/remove-keyword', function(req,res) {
+  const { keyword } = req.body;
+  const keywords = JSON.parse(fs.readFileSync('./data/keywords.json')).filter(kw => (kw !== keyword));
+
+  fs.writeFileSync('./data/keywords.json', JSON.stringify(keywords,null,2));
 
   res.status(200).send();
 });
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', columns: schema.map(col=>col.title) });
 });
 
+router.get('/keywords', function(req, res, next) {
+  const keywords = JSON.parse(fs.readFileSync('./data/keywords.json'));
+  res.render('keywords', { title: 'Express', keywords });
+});
+
+router.get('/schema', function(req, res, next) {
+  const schema = JSON.parse(fs.readFileSync('./data/schema.json'));
+  res.render('schema', { title: 'Express', columns: schema.map(col=>col.title) });
+});
+
+router.get('/urls', function(req, res, next) {
+  const schema = JSON.parse(fs.readFileSync('./data/schema.json'));
+  res.render('urls', { title: 'Express', columns: schema.map(col=>col.title) });
+});
+
 module.exports = router;
